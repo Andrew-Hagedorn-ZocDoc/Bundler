65310308058728e9e1a568aeef1ce2bd96d2cb37
Andrew Hagedorn <andrew.hagedorn@zocdoc.com>
2014-05-11 07:20:55 -0400

    this file is extraneous

--- src/jasmine-tests/test-cases/bundle-ab-files/test2.js
+++ src/jasmine-tests/test-cases/bundle-ab-files/test2.js
@@ -1,7 +1,12 @@
-;window["JST"] = window["JST"] || {}; JST['mustache3'] = new Hogan.Template({ code: function(c,p,i){var _=this;_.b(i=i||"");if(_.s(_.f("i18n",c,p,1),c,p,0,11,22,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("ls30.string");});c.pop();}_.b("\n" + i);if(_.s(_.f("i18n",c,p,1),c,p,0,45,56,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("ls31.string");});c.pop();}_.b("\n" + i);if(_.s(_.f("i18n",c,p,1),c,p,0,79,90,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("ls32.string");});c.pop();}_.b("\n");return _.fl();;}, partials: {}, subs: {} });
-;window["JST"] = window["JST"] || {}; JST['mustache4'] = new Hogan.Template({ code: function(c,p,i){var _=this;_.b(i=i||"");if(_.s(_.f("i18n",c,p,1),c,p,0,11,22,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("ls40.string");});c.pop();}_.b("\n" + i);if(_.s(_.f("i18n",c,p,1),c,p,0,45,56,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("ls41.string");});c.pop();}_.b("\n" + i);if(_.s(_.f("i18n",c,p,1),c,p,0,79,90,"{{ }}")){_.rs(c,p,function(c,p,_){_.b("ls42.string");});c.pop();}_.b("\n");return _.fl();;}, partials: {}, subs: {} });
-;// @localize js21.string
+;// @localize js11.string
 
-i18n.t('js22.string');
-i18n.t("js23.string");
+for(var i=0; i<12; i++)
+{console.log('nothing')}
+var x=12;
+x++;
+;// @localize js11.string
 
+AB.isOn('ab.config.4');
+var x=12;
+AB.isOn("ab.config.5");
+x++;
